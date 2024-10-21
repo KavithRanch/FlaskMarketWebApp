4cf80f4c2ee8035f390d62a8ddcc39046085f5cf
KavithRanch <kavithranch@gmail.com>
2024-10-21 17:12:50 -0400

    Connected HTML (Bootstrap styling) page with Flask backend

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # FlaskMarketWebApp
 **_Learning Flask through creating a market web app_**
 
+Learning Material: https://youtu.be/Qr4QMBUPxWo?si=uhDPh79dkLB_iInH
+
 ## Notes:
+### Setup
 - flask.palletsprojects.com/en/1.1x/quickstart provides starter code for Flask
+
+### Running Program
+
+'''
+// Setting python file as Flask App to run
+set FLASK_APP=market.py
+
+// Runs app
+python -m flask -run
+
+// Sets debug mode on
+// Changes can be made and seen without restarting App
+// Once deployed, turn it off
+set FLASK_DEBUG=1
+'''
--- market.py
+++ market.py
@@ -1,8 +1,20 @@
-from flask import Flask  # Importing Flask instance from flask package
+from flask import Flask, render_template  # Importing Flask instance from flask package
 app = Flask(__name__)  # Initializing Flask instance with parameter __name__ (
 
 
-# Route URL for home page
-@app.route('/')  # Decorator is where this function's content will appear
-def hello_world():  # Function which is used to display data on homepage
-    return 'Hello, World!'
+# # Route URL for home page
+# @app.route('/')  # Decorator is where this function's content will appear
+# def hello_world():  # Function which is used to display data on homepage
+#     return '<h1>Hello World</h1>'
+#
+#
+# # Dynamic Route URL for about page for any user
+# @app.route('/about/<username>')
+# def about_page(username):
+#     return f'<h1>This is the about page of {username}</h1>'
+
+# Route URLs for home page
+@app.route('/')
+@app.route('/home')
+def hello_world():
+    return render_template('home.html')  # Pointing to the html home page
