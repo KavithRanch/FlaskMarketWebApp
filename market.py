4836d8f3a66a86c6509529b941ce5bae6ad7a10f
Kavith Ranchagoda <kavith.10082797@ltimindtree.com>
2024-10-30 11:39:22 -0400

    Created new database and model for market items

--- market.py
+++ market.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template  # Importing Flask instance from flask package
-app = Flask(__name__)  # Initializing Flask instance with parameter __name__ (
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)  # Initializing Flask instance with parameter __name__
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'  # Config so App recognizes its databse
+db = SQLAlchemy(app) # Initializing DB
 
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
-# # Route URL for home page
+    def __repr__(self):
+        return f'Item {self.name}'
+# Route URL for home page
 # @app.route('/')  # Decorator is where this function's content will appear
 # def hello_world():  # Function which is used to display data on homepage
 #     return '<h1>Hello World</h1>'
@@ -22,9 +33,5 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)  # Pointing to the market home page
